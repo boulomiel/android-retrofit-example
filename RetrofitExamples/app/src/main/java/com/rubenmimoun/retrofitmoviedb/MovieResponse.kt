2304abba87d5174ef052db1978a1559c06f097c1
boulomiel <55102143+boulomiel@users.noreply.github.com>
2020-05-26 19:08:15 +0300

    Update MovieResponse.kt

--- RetrofitExamples/app/src/main/java/com/rubenmimoun/retrofitmoviedb/MovieResponse.kt
+++ RetrofitExamples/app/src/main/java/com/rubenmimoun/retrofitmoviedb/MovieResponse.kt
@@ -1,6 +1,12 @@
 package com.rubenmimoun.retrofitmoviedb
 
 
+// IF YOU USE THIS  => IllegalStateException: Expected BEGIN_OBJECT but was BEGIN_ARRAY at line 1 column 2 path $ 
+
+//data class MovieList (
+//    val list : List<Movie>
+//)
+
 data class Movie (
     val title: String,
     val image : String,
